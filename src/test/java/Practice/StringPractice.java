e8e1fe666bb1552ec392cdcc3c40f0a4d8a4518d
shahariyr reza <shahariyr.reza@hotmail.com>
2022-12-17 23:28:59 +0600

    test

---
+++ src/test/java/Practice/StringPractice.java
@@ -0,0 +1,13 @@
+
+package Practice;
+
+/**
+ *
+ * @author shahariyr reza
+ */
+public class StringPractice {
+    public static void main(String[] args) {
+        String s1 = "Shahariyr reza";
+        System.out.println("S1 is :"+s1);
+    }
+}
