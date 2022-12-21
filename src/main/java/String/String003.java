9ca0b435a2817ae87de97cc9119781c1d7f61bdb
shahariyr reza <shahariyr.reza@hotmail.com>
2022-12-21 23:13:19 +0600

    add 3rd string class

---
+++ src/main/java/String/String003.java
@@ -0,0 +1,20 @@
+
+package String;
+
+/**
+ *
+ * @author shahariyr reza
+ */
+public class String003 {
+    public static void main(String[] args) {
+        String me = "     My Name is Shahariyr Reza";
+        System.out.println(me);
+        String tr = me.trim();
+        System.out.println(tr);
+        char ch = me.charAt(20);
+        System.out.println("Char at index 15 :"+ch);
+        int ind = me.indexOf("is");
+        System.out.println("Index of is :"+ind);
+        
+    }
+}
