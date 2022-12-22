823b100632456b21f4993847a9030e0472d9bee3
shahariyr reza <shahariyr.reza@hotmail.com>
2022-12-22 12:18:09 +0600

    modified class string003

--- src/main/java/String/String003.java
+++ src/main/java/String/String003.java
@@ -1,4 +1,3 @@
-
 package String;
 
 /**
@@ -6,15 +5,16 @@ package String;
  * @author shahariyr reza
  */
 public class String003 {
+
     public static void main(String[] args) {
         String me = "     My Name is Shahariyr Reza";
         System.out.println(me);
         String tr = me.trim();
         System.out.println(tr);
         char ch = me.charAt(20);
-        System.out.println("Char at index 15 :"+ch);
+        System.out.println("Char at index 15 :" + ch);
         int ind = me.indexOf("is");
-        System.out.println("Index of is :"+ind);
-        
+        System.out.println("Index of is :" + ind);
+
     }
 }
