05788e1e5509af2aeb1b41f969a55ca3fefa68cb
shahariyr reza <shahariyr.reza@hotmail.com>
2022-12-30 19:03:17 +0600

    modified with a new method
    method that have constructor

--- src/main/java/OOP/Teacher.java
+++ src/main/java/OOP/Teacher.java
@@ -9,12 +9,20 @@ public class Teacher {
     String name, gender;
     int id, age;
 
+    void SetInfo(int a, String b, String c, int d) {
+        id = a;
+        name = b;
+        gender = c;
+        age = d;
+
+    }
+
     void DisplayInfo() {
 
-        System.out.println("id :" +id);
-        System.out.println("Name :" +name);
-        System.out.println("Gender :" +gender);
-        System.out.println("Age :" +age);
-        System.out.println("\n \n");
+        System.out.println("id :" + id);
+        System.out.println("Name :" + name);
+        System.out.println("Gender :" + gender);
+        System.out.println("Age :" + age);
+        System.out.println("\n");
     }
 }
--- src/main/java/OOP/TestTeacher.java
+++ src/main/java/OOP/TestTeacher.java
@@ -1,4 +1,3 @@
-
 package OOP;
 
 /**
@@ -6,20 +5,19 @@ package OOP;
  * @author shaha
  */
 public class TestTeacher {
+
     public static void main(String[] args) {
+
         Teacher teacher1 = new Teacher();
-        teacher1.id = 1;
-        teacher1.name = "Shahariyr reza";
-        teacher1.gender = "male";
-        teacher1.age = 23;
+        teacher1.SetInfo(1, "shahariyr", "Male", 23);
         teacher1.DisplayInfo();
-        
-        
+
         Teacher teacher2 = new Teacher();
-        teacher2.id = 2;
-        teacher2.name = "Akash Mahmud";
-        teacher2.gender = "male";
-        teacher2.age = 25;
+        teacher2.SetInfo(2, "Md Akash", "Male", 25);
         teacher2.DisplayInfo();
+
+        Teacher teacher3 = new Teacher();
+        teacher3.SetInfo(3, "Catrinaa Kyfe", "Female", 32);
+        teacher3.DisplayInfo();
     }
 }
