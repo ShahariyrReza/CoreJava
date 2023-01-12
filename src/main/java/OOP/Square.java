c52d81abeaa986a33b62a86ecfb18c30c65fa199
shahariyr reza <shahariyr.reza@hotmail.com>
2023-01-12 21:12:17 +0600

    return method

---
+++ src/main/java/OOP/Square.java
@@ -0,0 +1,19 @@
+/*
+ * This method will return a value.
+ * 
+ */
+package OOP;
+
+/**
+ *
+ * @author shahariyr reza
+ */
+public class Square {
+    
+    int square(int value){
+    return value*value;
+    }
+    
+    
+}
+
---
+++ src/main/java/OOP/TestSquare.java
@@ -0,0 +1,14 @@
+
+package OOP;
+
+/**
+ *
+ * @author shahariyr reza
+ */
+public class TestSquare {
+    public static void main(String[] args) {
+        Square ob1 = new Square();
+        int result =ob1.square(5);
+        System.out.println("The result is :"+result);
+    }
+}
