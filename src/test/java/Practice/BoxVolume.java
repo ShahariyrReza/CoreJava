f2a7076040c399291f37629adf8ea36b01b3fc20
shahariyr reza <shahariyr.reza@hotmail.com>
2023-01-27 17:39:26 +0600

    create boxVolume

---
+++ src/test/java/Practice/BoxVolume.java
@@ -0,0 +1,17 @@
+
+package Practice;
+
+/**
+ *
+ * @author shaha
+ */
+public class BoxVolume {
+    public static void main(String[] args) {
+        Box b1 = new Box(10, 10, 10);
+        Box b2 = new Box(20, 30, 10);
+        
+        b1.displayVol();
+        b2.displayVol();
+        
+    }
+}
