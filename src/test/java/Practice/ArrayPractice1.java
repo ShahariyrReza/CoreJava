dfad691e0f2cb2bb102ba1401f20bbb80f003257
shahariyr reza <shahariyr.reza@hotmail.com>
2023-02-03 12:25:14 +0600

    add a class

---
+++ src/test/java/Practice/ArrayPractice1.java
@@ -0,0 +1,33 @@
+package Practice;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author shahariyr reza
+ * @created (03.03.23)
+ */
+public class ArrayPractice1 {
+
+    public static void main(String[] args) {
+
+        int n;
+        double sum = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("How Many Information Do you wanna Calclute? :");
+        n = input.nextInt();
+        double[] info = new double[n];
+
+        for (int i = 0; i < info.length; i++) {
+            System.out.println("Please Give the Info number " + (i+1));
+            info[i] = input.nextDouble();
+
+        }
+
+        for (int i = 0; i < info.length; i++) {
+            sum = sum + info[i];
+        }
+        System.out.println("The Sum is :" + sum);
+
+    }
+}
