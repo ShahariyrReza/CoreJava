5d1e0b197a6df928a5439e44a4468fd0b309eeff
shahariyr reza <shahariyr.reza@hotmail.com>
2023-02-25 23:40:08 +0600

    change series

--- src/main/java/basic_java/series.java
+++ src/main/java/basic_java/series.java
@@ -1,4 +1,3 @@
-
 package basic_java;
 
 import java.util.Scanner;
@@ -8,17 +7,20 @@ import java.util.Scanner;
  * @author shahariyr reza
  */
 public class series {
+
     public static void main(String[] args) {
+
+        System.out.println("Enter the last number:");
         Scanner input = new Scanner(System.in);
-        int num,sum=0;
-         System.out.print("Enter the last number:");
-        num=input.nextInt();
         
+        int num, sum = 0;
+        num = input.nextInt();
+
         for (int i = 0; i <= num; i++) {
-            sum=sum+i;
-            System.out.print(i+"+");
+            sum = sum + i;
+            System.out.print(i + "+");
         }
-        System.out.println("\nThe sum of given number is :"+sum);
-        
+        System.out.println("\nThe sum of given number is :" + sum);
+
     }
 }
