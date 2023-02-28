ba9bded98e1503ef274c3d1816a45e35ec2ad677
shahariyr reza <shahariyr.reza@hotmail.com>
2023-02-28 23:10:01 +0600

    New class add

---
+++ src/main/java/OOP/Person.java
@@ -0,0 +1,22 @@
+
+package OOP;
+
+/**
+ *
+ * @author Shahariyr Reza
+ */
+public class Person {
+    private String name;
+    private int age;
+    
+    public void setName(String name){
+    this.name = name;
+    
+    }
+    
+    public String getName(){
+    return name;
+    
+    }
+    
+}
---
+++ src/main/java/OOP/PersonTest.java
@@ -0,0 +1,16 @@
+
+package OOP;
+
+/**
+ *
+ * @author shahariyr
+ */
+public class PersonTest {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        
+        p1.setName("Shahariyr Reza");
+        
+        System.out.println(p1.getName());
+    }
+}
