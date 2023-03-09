5908c99245a35e82603f5fe332b761b2bf1bbe93
shahariyr reza <shahariyr.reza@hotmail.com>
2023-03-09 21:25:21 +0600

    update

--- src/main/java/Keyword/SuperKeyword01.java
+++ src/main/java/Keyword/SuperKeyword01.java
@@ -2,4 +2,13 @@ package Keyword;
 
 public class SuperKeyword01 {
     int x = 10;
+    void Display(){
+        System.out.println("Hasib");
+    }
+
+    public SuperKeyword01() {
+        System.out.println("Hello");
+    }
+    
 }
+ 
--- src/main/java/Keyword/SuperKeyword02.java
+++ src/main/java/Keyword/SuperKeyword02.java
@@ -3,7 +3,10 @@ package Keyword;
 
 public class SuperKeyword02 extends SuperKeyword01 {
     int x = 20;
+    @Override
     void Display(){
+        
+        super.Display();
         System.out.println(super.x);
     }
 }
