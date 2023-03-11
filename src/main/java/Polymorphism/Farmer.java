8aec850111f57bbd228a1ec9be40047b96897fbc
shahariyr reza <shahariyr.reza@hotmail.com>
2023-03-12 00:03:09 +0600

    update farmer

--- src/main/java/Polymorphism/Farmer.java
+++ src/main/java/Polymorphism/Farmer.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Polymorphism;
 
-/**
- *
- * @author shaha
- */
-public class Farmer {
-    
+public class Farmer extends Teacher {
+
+    @Override
+    void Display() {
+        System.out.println("I am a Farmer");
+    }
 }
