010dcaa09ed0528d630c87623d757bab6720b219
shahariyr reza <shahariyr.reza@hotmail.com>
2023-03-12 00:03:35 +0600

    update teacher

--- src/main/java/Polymorphism/Teacher.java
+++ src/main/java/Polymorphism/Teacher.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Polymorphism;
 
-/**
- *
- * @author shaha
- */
-public class Teacher {
-    
+public class Teacher extends Person {
+
+    @Override
+    void Display() {
+        System.out.println("I am a Teacher");
+    }
 }
