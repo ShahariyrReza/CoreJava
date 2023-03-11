dd4df19106b7a2db74e3e118e170c4d8601bcfd7
shahariyr reza <shahariyr.reza@hotmail.com>
2023-03-12 00:03:25 +0600

    update person

--- src/main/java/Polymorphism/Person.java
+++ src/main/java/Polymorphism/Person.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Polymorphism;
 
-/**
- *
- * @author shaha
- */
 public class Person {
-    
+
+    void Display() {
+        System.out.println("I am a person");
+    }
 }
