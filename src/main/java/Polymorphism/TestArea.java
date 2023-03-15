4a8319ff6e05fcce6ecc0e8b6cf9de6ecc29c451
shahariyr reza <shahariyr.reza@hotmail.com>
2023-03-15 23:00:04 +0600

    update rectangle

--- src/main/java/Polymorphism/TestArea.java
+++ src/main/java/Polymorphism/TestArea.java
@@ -5,5 +5,18 @@ package Polymorphism;
  * @author shahariyr reza
  */
 public class TestArea {
-    
+
+    public static void main(String[] args) {
+
+        Area s1 = new Area();
+        s1.area();
+        
+        Area s2 = new  Tringle(10, 10);
+        System.out.println(s2.area());
+        
+        Area s3 = new Rectangle(10,3);
+        double x = s3.area();
+        System.out.println("X :"+x);
+        
+    }
 }
