a947e515ac45abeafbdba511cf07c0cb6fcd47e6
shahariyr reza <shahariyr.reza@hotmail.com>
2023-03-15 23:32:01 +0600

    test class

---
+++ src/main/java/Abstraction/Test.java
@@ -0,0 +1,12 @@
+package Abstraction;
+
+public class Test {
+    public static void main(String[] args) {
+        MobileUser m1;
+        m1= new Rahim();
+        m1.SendMessage();
+        
+        m1 = new Karim();
+        m1.SendMessage();
+    }
+}
