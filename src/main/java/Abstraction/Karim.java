57a34aed14e79dd49271e7173f64f515d6013827
shahariyr reza <shahariyr.reza@hotmail.com>
2023-03-15 23:31:26 +0600

    class karim

---
+++ src/main/java/Abstraction/Karim.java
@@ -0,0 +1,10 @@
+package Abstraction;
+
+public class Karim extends MobileUser {
+
+    @Override
+    void SendMessage() {
+
+        System.out.println("Hello Rahim. I'm Fine,and you?");
+    }
+}
