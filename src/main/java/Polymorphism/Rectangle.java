c8c2166d1b68d9f65b5e7edc4aaad913c53f56b7
shahariyr reza <shahariyr.reza@hotmail.com>
2023-03-15 23:00:17 +0600

    update rectangle

--- src/main/java/Polymorphism/Rectangle.java
+++ src/main/java/Polymorphism/Rectangle.java
@@ -1,5 +1,18 @@
 package Polymorphism;
 
-public class Rectangle {
-    
+public class Rectangle extends Tringle {
+
+    double length, width;
+
+   
+    public Rectangle(double length, double width) {
+
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double area() {
+        return length * width;
+    }
 }
