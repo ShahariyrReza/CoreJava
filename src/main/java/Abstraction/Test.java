8e479db93055e648531d85484a405695eb5238c3
shahariyr reza <shahariyr.reza@hotmail.com>
2023-03-17 23:19:12 +0600

    update abstraction

--- src/main/java/Abstraction/Test.java
+++ src/main/java/Abstraction/Test.java
@@ -1,11 +1,13 @@
 package Abstraction;
 
 public class Test {
+
     public static void main(String[] args) {
         MobileUser m1;
-        m1= new Rahim();
-        m1.SendMessage();
         
+        m1 = new Rahim();
+        m1.SendMessage();
+
         m1 = new Karim();
         m1.SendMessage();
     }
