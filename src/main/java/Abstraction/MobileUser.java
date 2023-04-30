e240989315df0643c1c30267c2e469a88e851b5a
shahariyr reza <shahariyr.reza@hotmail.com>
2023-04-30 20:58:44 +0600

    update mobileuser

--- src/main/java/Abstraction/MobileUser.java
+++ src/main/java/Abstraction/MobileUser.java
@@ -1,6 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Abstraction;
 
-public abstract class MobileUser {
-
-    abstract void SendMessage();
+/**
+ *
+ * @author shaha
+ */
+public class MobileUser {
+    
 }
