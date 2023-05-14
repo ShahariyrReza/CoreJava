de6e357afb432dabe840de0942555573cdb9238e
shahariyr reza <shahariyr.reza@hotmail.com>
2023-05-14 22:59:01 +0600

    add a class

--- src/main/java/Abstraction/MobileUser.java
+++ src/main/java/Abstraction/MobileUser.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Abstraction;
 
-/**
- *
- * @author shaha
- */
-public class MobileUser {
-    
+
+public abstract class MobileUser {
+   abstract void  sendMessage();
 }
