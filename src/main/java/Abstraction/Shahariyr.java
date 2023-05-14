04e68515715e7cfcfe7892fc785d1a20e8c076a0
shahariyr reza <shahariyr.reza@hotmail.com>
2023-05-14 22:59:24 +0600

    add a class

--- src/main/java/Abstraction/Shahariyr.java
+++ src/main/java/Abstraction/Shahariyr.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Abstraction;
 
-/**
- *
- * @author shaha
- */
-public class Shahariyr {
-    
+
+public class Shahariyr extends MobileUser{
+   @Override
+    void sendMessage(){
+       System.out.println("Hello , I'm Shahariyr Reza");
+    }
 }
