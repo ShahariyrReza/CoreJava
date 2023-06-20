2e2fc51cf9a462bd0a77a0c471f49fa720936cd3
shahariyr reza <shahariyr.reza@hotmail.com>
2023-06-20 23:18:34 +0600

    add tringle

--- src/main/java/problemSolving/Triangle.java
+++ src/main/java/problemSolving/Triangle.java
@@ -1,7 +1,15 @@
-
 package problemSolving;
 
+public class Triangle extends Shape {
 
-public class Triangle {
-    
+    public Triangle(double base, double height) {
+        super(base, height);
+    }
+
+    @Override
+    void area() {
+        double result = 0.5 * dim1 * dim2;
+        System.out.println("Area of Triangle is \n" + result);
+    }
+
 }
