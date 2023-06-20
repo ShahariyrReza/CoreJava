1c69d70fec2907bb1071d713ff39211974ee5975
shahariyr reza <shahariyr.reza@hotmail.com>
2023-06-20 23:18:20 +0600

    add circle

--- src/main/java/problemSolving/Circle.java
+++ src/main/java/problemSolving/Circle.java
@@ -1,7 +1,15 @@
-
 package problemSolving;
 
+public class Circle extends Shape {
 
-public class Circle {
-    
+    public Circle(double r) {
+        super(r, r);
+    }
+
+    @Override
+    void area() {
+        double result = Math.PI * dim1 * dim2;
+        System.out.println("Circle area is \n"+ result +"\n");
+    }
+
 }
