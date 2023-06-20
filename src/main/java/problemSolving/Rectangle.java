f6a21e0e8663b9e97cb34e3351a5ea6f2476e022
shahariyr reza <shahariyr.reza@hotmail.com>
2023-06-20 23:19:00 +0600

    add rectangle

--- src/main/java/problemSolving/Rectangle.java
+++ src/main/java/problemSolving/Rectangle.java
@@ -1,7 +1,15 @@
-
 package problemSolving;
 
+public class Rectangle extends Shape {
 
-public class Rectangle extends Shape{
-    
+    Rectangle(double dim1, double dim2) {
+        super(dim1, dim2);
+    }
+
+    @Override
+    void area() {
+
+        double result = dim1 * dim2;
+        System.out.println("Area of Rectangle is \n" + result+"\n");
+    }
 }
--- src/main/java/problemSolving/TestApplication.java
+++ src/main/java/problemSolving/TestApplication.java
@@ -6,5 +6,16 @@ package problemSolving;
  * @author Shahariyr reza
  */
 public class TestApplication {
-    
+    public static void main(String[] args) {
+        Shape sh;
+        
+        sh = new Rectangle(10, 20);
+        sh.area();
+        
+        sh = new Circle(5);
+        sh.area();
+        
+        sh = new Triangle(10, 10);
+        sh.area();
+    }
 }
